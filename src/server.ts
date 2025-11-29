c42b8502b8e90e410a96450927d8c7806074a36d
amitsengupta <amitsengupta332@gmail.com>
2025-11-30 00:48:57 +0600

    Solve the previous error, add .env & connect with GitHub

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+
 const app = express();
 const port = 5000;
 
@@ -7,32 +13,46 @@ const port = 5000;
 app.use(express.json());
 
 //DB
-// const pool = new Pool({
-//   connectionString: `${process.env.CONNECTION_STR}`,
-// });
 const pool = new Pool({
-  connectionString: `psql 'postgresql://neondb_owner:npg_xQp2mYVJiZ4b@ep-round-river-aden8ef1-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'`,
+  connectionString: `${process.env.CONNECTION_STR}`,
 });
+// const pool = new Pool({
+//   connectionString:
+//     "",
+// });
 
 const initDB = async () => {
   await pool.query(
     `
     CREATE TABLE IF NOT EXISTS users(
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(100) NOT NULL,
-          email VARCHAR(100) NOT NULL,
-          age INT ,
-          phone VARCHAR(15),
-          address TEXT,
-          created_at TIMESTAMP DEFAULT NOW(),
-          updated_at TIMESTAMP DEFAULT NOW()
+       id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
  
     )
     `
   );
+
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
 };
 
-// initDB();
+initDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
