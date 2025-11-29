beca02448dfe2dc9609ffe936b47b8eaf8c61293
amitsengupta <amitsengupta332@gmail.com>
2025-11-29 22:52:44 +0600

    Installing Postgres as database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { Pool } from "pg";
 const app = express()
 const port = 5000
+
+// parser
+app.use(express.json());
+
+//DB
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.post('/', (req:Request, res:Response) => {
+ 
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
