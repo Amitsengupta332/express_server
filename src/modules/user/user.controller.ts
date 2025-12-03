3daab522c3e2f15e0e7bb672069c98c0c2446d45
amitsengupta <amitsengupta332@gmail.com>
2025-12-04 00:23:38 +0600

    Rearranging the User Module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,11 +6,8 @@ const createUser = async (req: Request, res: Response) => {
   const { name, email } = req.body;
 
   try {
-    const result = await pool.query(
-      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
-      [name, email]
-    );
-    // console.log(result.rows[0]);
+   const result = await userServices.createUser(name, email);
+
     res.status(201).json({
       success: false,
       message: "Data Instered Successfully",
@@ -26,7 +23,7 @@ const createUser = async (req: Request, res: Response) => {
 
 const getUser = async (req: Request, res: Response) => {
   try {
-    const result = await pool.query(`SELECT * FROM users`);
+    const result = await userServices.getUser();
 
     res.status(200).json({
       success: true,
@@ -45,9 +42,8 @@ const getUser = async (req: Request, res: Response) => {
 const getSingleUser = async (req: Request, res: Response) => {
   // console.log(req.params.id);
   try {
-    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
-      req.params.id,
-    ]);
+   
+    const result = await userServices.getSingleUser(req.params.id as string);
 
     if (result.rows.length === 0) {
       res.status(404).json({
@@ -73,11 +69,8 @@ const updateUser = async (req: Request, res: Response) => {
   // console.log(req.params.id);
   const { name, email } = req.body;
   try {
-    const result = await userServices.createUser(name, email);
-    // const result = await pool.query(
-    //   `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
-    //   [name, email, req.params.id]
-    // );
+     
+    const result = await userServices.updateUser(name, email, req.params.id as string);
 
     if (result.rows.length === 0) {
       res.status(404).json({
@@ -102,10 +95,8 @@ const updateUser = async (req: Request, res: Response) => {
 const deleteUser = async (req: Request, res: Response) => {
   // console.log(req.params.id);
   try {
-    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
-      req.params.id,
-    ]);
-
+     
+    const result = await userServices.deleteUser(req.params.id as string);
     if (result.rowCount === 0) {
       res.status(404).json({
         success: false,
